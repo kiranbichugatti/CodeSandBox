61dc7fad7d5654a38aa13a7fc07b1913476b98c1
Bichugatti <Bichugatti@BICHUGATTI>
2020-05-24 20:50:43 -0500

    Best Time to Buy Stock and Sell

---
+++ BestTimeToBuyAndSellStock/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BestTimeToBuyAndSellStock
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] inputArr = { 7, 1, 5, 3, 6, 4 };
+            Console.WriteLine(MaxProfit(inputArr));
+        }
+        public static int MaxProfit(int[] prices)
+        {
+            if (prices == null || prices.Length == 0) return 0;
+            var maximumValue = 0;
+            var currentValue = prices[0];
+            for (int i = 1 ; i < prices.Length; i++)
+            {
+                if (prices[i]<currentValue)
+                {
+                    currentValue = prices[i];
+                }
+                else
+                {
+                    maximumValue = Math.Max(maximumValue, prices[i] - currentValue);
+                }
+            }
+            return maximumValue;
+        }
+
+    }
+}
+
