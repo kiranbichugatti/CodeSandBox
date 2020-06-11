73aea8fae347ad274d522df0f6ea9b21951e7547
Bichugatti <Bichugatti@BICHUGATTI>
2020-06-10 23:55:37 -0500

    Factorial of a number

---
+++ Factorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter an integer");
+            var input = Console.ReadLine();
+            int output = Factorial(Convert.ToInt32(input));
+            Console.WriteLine($"Factorial of {input} is : {output}");
+        }
+
+        public static int Factorial(int n)
+        {
+            if (n < 0) return -1;
+
+            if (n == 1) return 1;
+
+            return n * Factorial(n - 1);
+        }
+    }
+}
