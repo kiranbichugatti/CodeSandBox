f9823d3ca4a57e49e8c28a59419186b933dc0419
Kiran Bichugatti <kiran.bichugatti@gmail.com>
2020-06-13 01:12:29 -0500

    * CheckUniqueCharInString.csproj: Check If unique Character in   String
    * Program.cs:

---
+++ CheckUniqueCharInString/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace CheckUniqueCharInString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string!");
+            var str = Console.ReadLine();
+            var output = IsUniqueN(str);
+            Console.WriteLine(output);
+
+        }
+
+        public static bool IsUniqueNSquare(string s)
+        {
+            for(int i = 0;i < s.Length; i++)
+            {
+                for (int j = i + 1; j < s.Length; j++)
+                {
+                     if(s[i] == s[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        public static bool IsUniqueN(string s)
+        {
+            if (s.Length > 128) return false;
+
+            bool[] flag = new bool[128];
+
+            foreach(char c in s)
+            {
+                if(flag[c])
+                {
+                    return false;
+                }
+                flag[c] = true;
+            }
+
+            return true;
+        }
+    }
+}
